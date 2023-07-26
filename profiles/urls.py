57646e9681b19f72758771076160fbcabfdfde79
dEEGE2time <johannesweidensjo@hotmail.com>
2023-07-26 13:32:39 +0000

    add profiles detail view

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,6 @@ from profiles import views
 
 urlpatterns = [
     path('profiles/', views.ProfileList.as_view()),
+    # from views.py implement this to be able to search for specific profiles with id.
+    path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
 ]
